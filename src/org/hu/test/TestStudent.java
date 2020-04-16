244157c3833c80624f22630d28e183802857c27b
zeroxes <1369165806@qq.com>
2020-04-16 10:25:59 +0800

    ioc 快速上手 移除 bin/ 目录上传

--- src/org/hu/test/TestStudent.java
+++ src/org/hu/test/TestStudent.java
@@ -1,5 +1,28 @@
 package org.hu.test;
 
-public class TestStudent {
+import org.hu.entity.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+public class TestStudent {
+	
+	public static void main(String[] args) {
+		createStudentByCommon();
+		createStudentByIOC();
+	}
+
+	private static void createStudentByCommon() {
+		Student student = new Student();
+		student.setNo("s-001");
+		student.setName("zhangsan");
+		student.setAge(21);
+		System.out.println(student);
+	}
+	
+	private static void createStudentByIOC() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Student student = (Student)context.getBean("stu_1");
+		System.out.println(student);
+	}
+	
 }
